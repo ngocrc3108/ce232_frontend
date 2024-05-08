100062c4ca27b288a3eea75119e051de94f4a083
ngocrc <tienngoc2003dh@gmail.com>
2024-05-08 11:52:52 +0700

    lam login form tu mui

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,16 +1,37 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { serverUrl } from "../App";
 import { myFetch } from "../App";
 
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function Login({setLoggedIn}) {
-    const [username, setUsername] = useState(() => "");
-    const [password, setPassowrd] = useState(() => "");
+// TODO remove, this demo shouldn't need to reset the theme.
+
+const defaultTheme = createTheme();
+
+export default function Login({setLoggedIn}) {
     const [errMessage, setErrMessage] = useState(() => "");
     const navigate = useNavigate();
 
-    const onButtonClick = async () => {
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        const username = data.get('username')
+        const password = data.get('password')
+        console.log({
+            username,
+            password,
+        });
         const {success, message} = await myFetch('/auth/login', {
             body : {username, password}
         })
@@ -20,27 +41,66 @@ function Login({setLoggedIn}) {
 
         if(success)
             navigate("/")
-    }
+  };
 
-    return (
-        <div>
-            <input
-                value={username}
-                placeholder="username"
-                onChange={(ev) => setUsername(ev.target.value)}
-                className={"inputBox"}
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="username"
+              name="username"
+              autoComplete="username"
+              autoFocus
             />
-            <input
-                value={password}
-                placeholder="password"
-                onChange={(ev) => setPassowrd(ev.target.value)}
-                className={"inputBox"}
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
             />
-            <p>{errMessage}</p>
-            <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'} />
-        </div>
-
-    );
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <p>{errMessage}</p>
+      </Container>
+    </ThemeProvider>
+  );
 }
-
-export default Login;
